083964f927f5790a593eb6e47688f3313a414ba1
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-03-05 15:06:44 +0900

    탱크가 벽보면 회전함
    ㅋㅋ...

---
+++ Assets/DD/State.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    private void Enter()
+    {
+    }
+}
--- Assets/TankAI.cs
+++ Assets/TankAI.cs
@@ -19,5 +19,17 @@ namespace DD
             headTransform.rotation = Quaternion.EulerRotation(0, 45, 0);
             Instantiate(bullet, Po);
         }
+
+        private void Update()
+        {
+            if (Physics.Raycast(this.transform.position, transform.forward, 3f))
+            {
+                transform.rotation = transform.rotation * Quaternion.EulerRotation(0, 90, 0);
+            }
+            else
+            {
+                transform.Translate(new Vector3(0, 0, 1f * Time.deltaTime));
+            }
+        }
     }
 }
